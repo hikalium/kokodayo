92c693e83e59282f32b16cbf767c52642d277533
hikalium <hikalium@hikalium.com>
2020-07-12 14:10:41 +0900

    Add static file serving

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"goji.io"
 	"goji.io/pat"
@@ -14,9 +16,19 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, kokodayo.GenHello(pat.Param(r, "name")))
 }
 
+const host = "localhost"
+const port = 8080
+const staticPath = "./static"
+
 func main() {
 	mux := goji.NewMux()
 	mux.HandleFunc(pat.Get("/hello/:name"), hello)
+	path, err := filepath.Abs(staticPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mux.Handle(pat.Get("/*"), http.FileServer(http.Dir(path)))
 
-	http.ListenAndServe("localhost:8000", mux)
+	fmt.Printf("Listening %v:%v...\n", host, port)
+	http.ListenAndServe(fmt.Sprintf("%v:%v", host, port), mux)
 }
